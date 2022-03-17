85842a0ab8f5412ba9d136d7217de9156501f088
w1317 <xmjj1317@gmail.com>
2022-03-17 12:07:49 +0800

    Update Arknights_default.py

--- Arknights_default.py
+++ Arknights_default.py
@@ -1,9 +1,11 @@
 # 请将此脚本、img文件夹和ResourceDictionary.py文件复制到项目根目录下再运行！
 
-import ADBHelper, RaphaelScriptHelper, ResourceDictionary
+import ADBHelper
+import RaphaelScriptHelper
+import ResourceDictionary
 
 deviceList = ADBHelper.getDevicesList()
-#print(deviceList)
+# print(deviceList)
 '''i = 0
 for did in deviceList:
     print(str(i) + ": " + did)
@@ -14,11 +16,22 @@ delayTime = input("请输入关卡所耗时间（单位：秒）\n")'''
 
 RaphaelScriptHelper.deviceType = 1
 # RaphaelScriptHelper.deviceID = deviceList[int(input_i)]
-RaphaelScriptHelper.deviceID ="cda135e9"
-delayTime = 85
+RaphaelScriptHelper.deviceID = "cda135e9"
+
+
+class Operation(object):
+    def __init__(self, id, duration, description=''):
+        self.id = id
+        self.duration = duration
+        self.description = description
+
+
+operation_info = {'1-7': Operation('1-7', 85, '刷固源岩'), 'GA-8': Operation('GA-8', 160)}
+
+delayTime = operation_info['GA-8'].duration
 retry_times = 3
 
-for i in range(0,100):
+for i in range(0, 100):
     j = 0
     while j < retry_times:
         RaphaelScriptHelper.find_pic_touch(ResourceDictionary.start_QHD)
